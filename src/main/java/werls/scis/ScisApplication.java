1c55f4fc5adb6178dc247e8aa43eb754d12ed76f
gsdukbh <gsdukbh@gmail.com>
2020-02-19 12:22:48 +0800

    Initial commit

---
+++ src/main/java/werls/scis/ScisApplication.java
@@ -0,0 +1,13 @@
+package werls.scis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/werls/scis/ScisApplicationTests.java
@@ -0,0 +1,13 @@
+package werls.scis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
